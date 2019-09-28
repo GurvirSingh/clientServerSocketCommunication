e2c8e39b39404aad863a254bee379c4f756b35dc
Gurvir Singh Bhogal <40941950+GurvirSingh@users.noreply.github.com>
2019-09-28 15:42:47 -0500

    Add files via upload

--- Client.java
+++ Client.java
@@ -3,85 +3,83 @@ import java.io.*;
 
 public class Client {
 
+// Declare all the input and output buffers as null
+
 	private Socket socket = null;
 	private DataInputStream input, input1, in = null;
 	private DataOutputStream out = null;
 
-	public Client(String address, int port) {
+// Parameterized Constructor for Client class
 
+	public Client(String address, int port) {
+		
 		try {
+			// establish socket connection
 			socket = new Socket(address, port);
 			System.out.println("Connection established");
-		
+			
+			// Initialise the instance variables for communication
 			input = new DataInputStream(System.in);
 			out = new DataOutputStream(socket.getOutputStream());
 			in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 
 			// get command from user
 			System.out.println("\nEnter UPLOAD to upload a file to the server \nDOWNLOAD to download a file from the server\nRENAME to rename a file in the server\nDELETE to delete a file from the server. [All commands should be in Upper Case]");
-
 			input1 = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-		
 			String command = " ";
-			
-			
 			System.out.println("\n\nEnter Command: ");
 			command = input.readLine();
 
-			if(command.equals("UPLOAD")) 
-			{
-
+			// check command for validations 
+			if(command.equals("UPLOAD")) {
+				//send command to server 
 				out.writeUTF(command);				
 
+				//read acknowledgement from server
 				String incommand = input1.readUTF();
 				System.out.println(incommand);
-
+				
+				// get filename from user
 				System.out.println("\nEnter File name to Upload: ");
 				String file_name = "";
 				file_name = input.readLine();
 				
+				// initialise buffer for communication
 				BufferedReader br = new BufferedReader(new FileReader(file_name)); 
+
+				// send filename to the server
   				out.writeUTF(file_name);
-
+	
+				// send the file block by block
   				String st = ""; 
-
-
   				while ((st = br.readLine()) != null) {
-				//System.out.println(st); 
+				// below line commented for debugging purposes
+				// System.out.println(st); 
 				out.writeUTF(st); 
     				}
 				out.writeUTF("EOF"); 
-				out.writeUTF("Over");
 			}
-			//else {
-			//	System.out.println("\nError: Command not found");
-			//}
-			
 			else if(command.equals("DOWNLOAD")) {
-
+				//send command to server 
 				out.writeUTF(command);
 
 				String incommand = input1.readUTF();
 				System.out.println(incommand);
 
 				System.out.println("Enter file name to download:\n");
-				
-				String exists = input1.readUTF();
-				System.out.println(exists);
-			
-				if(incommand.equals("File not found!")) {
-					System.out.println("Error: File not found");	
-					socket.close();	
-				}
-				else {
 
 				String create_file_down1 = "";
 				create_file_down1 = input.readLine();
 
 				
-				String create_file_down2 ="D:/Java/gshub/Downloads_by_client/"+create_file_down1;
+				String create_file_down2 ="/home/gurvir/A/Work/CSE5306 Project1/Downloads_by_client/"+create_file_down1;
 
 				out.writeUTF(create_file_down1);
+
+				String exist = input1.readUTF();
+				// below line commented for debugging purposes
+				//System.out.println(exist);
+				if(exist.equals("TRUE")){
 				
 				File file = new File(create_file_down2);
                 		file.createNewFile();
@@ -92,33 +90,36 @@ public class Client {
 				String wrt = "";
 				while(!((wrt = in.readUTF()).equals("EOF"))) {
     				writer.write(wrt);
+				writer.write("\n");
+				// below line commented for debugging purposes
 				//System.out.println(wrt);
 				}
 				
     				writer.close();
 				System.out.println("\nSuccessfully Saved!");
-				socket.close();
+				// below line commented for debugging purposes
+				//socket.close();
 				
-				}//end of else
+				}//end of if
+				else {
+					System.out.println("File not found!");
+				}
+			
 			}
-				
 			// Rename 
-			else if(command.equals("RENAME")) 
-			{
+			else if(command.equals("RENAME")) {
+				//send command to server
 				out.writeUTF(command);
-
-				//String incommand = input1.readLine();
-				//System.out.println(incommand);
 
 				System.out.println("Enter File to Rename");
 				String file_name = "";
 				file_name = input.readLine();
 				out.writeUTF(file_name);
 
-				//BufferedReader br = new BufferedReader(new FileReader(file_name)); 
 				
 				  String eval = "";
 				  eval = input1.readUTF();
+				// below line commented for debugging purposes
 				  //System.out.println(eval);
 				  
 
@@ -141,15 +142,10 @@ public class Client {
 				  {
 					System.out.println("File Not Found");
 				  }
-
-
-
-
 			}
-
 			//Delete
-			else if(command.equals("DELETE")) 
-			{
+			else if(command.equals("DELETE")) {
+				//send command to server
 				out.writeUTF(command);
 
 				String incommand = input1.readUTF();
@@ -159,10 +155,7 @@ public class Client {
 				String file_name = "";
 				file_name = input.readLine();
 				out.writeUTF(file_name);
-
-				//BufferedReader br = new BufferedReader(new FileReader(file_name)); 
-				  
-				
+	  				
 				  String eval = "0";
 				  eval = input1.readUTF();
 
@@ -174,18 +167,17 @@ public class Client {
 				  {
 					  System.out.println("File not found");
 					
-				  }
-				  
-
+				  }				  
 			}
-			
+			else {
+				System.out.println("Command not found!");
+			}
+
 		}
-		catch(FileNotFoundException e) 
-		{			
+		catch(FileNotFoundException e) {
 			System.out.println("Error: File not found");
 		}
-		catch(Exception u) 
-		{
+		catch(Exception u) {
 			System.out.println("Error while establishing connection");
 		}
 		
--- Server.java
+++ Server.java
@@ -1,7 +1,6 @@
 import java.net.*;
 import java.io.*;
 
-
 public class Server {
 
 	private Socket socket = null;
@@ -10,7 +9,7 @@ public class Server {
 	private DataOutputStream ou = null;
 
 	public Server(int port) {
-
+		
 		try {
 			server = new ServerSocket(port);
 			System.out.println("Waiting for Connection");
@@ -23,10 +22,9 @@ public class Server {
 
 			String command = "";
 
-
 			command = in.readUTF();
 			//System.out.println(command);
-			
+				
 			if(command.equals("UPLOAD")){
 			// Send ok msg to client
 			System.out.println("Upload acknowledgement sent: OK");
@@ -36,7 +34,7 @@ public class Server {
 			System.out.println("\nClient is uploading a file...");
 			String file_name = "";
   			file_name = in.readUTF(); 
-			String create_file = "D:/Java/gshub/Uploads_by_client/"+file_name;
+			String create_file = "/home/gurvir/A/Work/CSE5306 Project1/Uploads_by_client/"+file_name;
 			try {
 				//String contents = in.readUTF();
                 		File file = new File(create_file);
@@ -48,19 +46,19 @@ public class Server {
 				String wrt = "";
 				while(!((wrt = in.readUTF()).equals("EOF"))) {
     				writer.write(wrt);
+				writer.write("\n");
 				//System.out.println(wrt);
 				}
 				
     				writer.close();
 				System.out.println("\nSuccessfully Saved!");
-				socket.close();
+				//socket.close();
 			
                 	}
                 	catch(Exception e) {
                 		System.out.println("Error Occured!");
                 	}
 			} // end of if condition 
-			
 			if(command.equals("DOWNLOAD")) {
 
 				System.out.println("Download acknowledgement sent: OK");
@@ -70,35 +68,19 @@ public class Server {
 				
 				file_name = in.readUTF();
 
-				// Check if file exists or not
-				File t = new File("/home/gurvir/A/Work");
-				boolean exists = t.exists();
-				System.out.println(exists);
-
-				if (!(exists)) {
-					ou.writeUTF("File not found!");
-				}
-				else {
-					ou.writeUTF("File found!");
-				}
-				
-
 				BufferedReader br_down = new BufferedReader(new FileReader(file_name)); 
 
   				String st = ""; 
 
-
+				ou.writeUTF("TRUE");
   				while ((st = br_down.readLine()) != null) {
-				System.out.println(st); 
+				//System.out.println(st); 
 				ou.writeUTF(st); 
     				}
 				ou.writeUTF("EOF"); 
-				ou.writeUTF("Over");
 			}
-				
 			// Rename 
-			if(command.equals("RENAME")) 
-			{
+			if(command.equals("RENAME")) {
 				//Send OK MSG TO CLIENT
 				System.out.println("Rename Acknowledgement sent :OK");
 				//ou.writeUTF("OK");
@@ -110,7 +92,7 @@ public class Server {
 				String file_name = "";
 				file_name = in.readUTF();
 				System.out.println("File Name = "+ file_name);
-				String create_file = "D:/Java/gshub/Uploads_by_client/"+file_name;
+				String create_file = "/home/gurvir/A/Work/CSE5306 Project1/Uploads_by_client/"+file_name;
 				File file = new File(create_file);
 
 				if(file.exists())
@@ -127,7 +109,7 @@ public class Server {
 					String nfile_name = "";
 					nfile_name = in.readUTF();
 					System.out.println("New File Name = " + nfile_name);
-					String ncreate_file = "D:/Java/gshub/Uploads_by_client/"+nfile_name;
+					String ncreate_file = "/home/gurvir/A/Work/CSE5306 Project1/Uploads_by_client/"+nfile_name;
 			     	File nfile = new File(ncreate_file);
 
 					if(file.renameTo(nfile))
@@ -136,10 +118,6 @@ public class Server {
 						ou.writeUTF(eval);
 						System.out.println("File Renamed");
 					}
-					
-					
-
-
 				}
 				else
 				{
@@ -147,8 +125,7 @@ public class Server {
 					ou.writeUTF(eval);
 					System.out.println("File Not Found");
 				}
-			}
-			
+			}		
 			//Delete
 			if(command.equals("DELETE")) {
 				//Send OK MSG TO CLIENT
@@ -161,7 +138,7 @@ public class Server {
 				String file_name = "";
 				file_name = in.readUTF();
 				System.out.println("File Name = "+ file_name);
-				String create_file = "D:/Java/gshub/Uploads_by_client/"+file_name;
+				String create_file = "/home/gurvir/A/Work/CSE5306 Project1/Uploads_by_client/"+file_name;
 				File file = new File(create_file);
 
 				if(file.exists())
@@ -180,19 +157,22 @@ public class Server {
 					ou.writeUTF(eval);
 
 				}
-				
-				
 				socket.close();
-
 			}
-			
 		}
 		catch(FileNotFoundException s) {
+		try{			
+			ou.writeUTF("FALSE");
 			System.out.println("Error: File not Found");		
 		}
-		catch(Exception e) {
-			System.out.println("Error in establishing connection");
+		catch (IOException i) {
+			System.out.println("Error");
 		}
+		}
+		catch(Exception e) {
+			System.out.println("Error in establishing connection" + e);
+		}
+	
 	}
 
 	public static void main(String args[]) {
